709d1df0e6ad7dae26f889fcaffdd893ed6917c7
bizk <c.s.yanzon@gmail.com>
2024-07-20 21:15:02 -0300

    added client logic and logging

--- src/client.rs
+++ src/client.rs
@@ -1,23 +1,148 @@
-// use zkp_auth::zkp_client::ZkpClient;
-// use zkp_auth::ParamsRequest;
+use num_bigint::{BigInt, RandBigInt, Sign};
+use rand::thread_rng;
+use std::error::Error;
+use tonic::transport::Channel;
+use zkp_auth::zkp_client::ZkpClient;
+use zkp_auth::{ParamsRequest, RegisterRequest, ChallengeRequest, SecretRequest};
 
 mod zkp_auth;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // channel connection to server
-    // let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
-    //     .connect()
-    //     .await?;
-    // let mut client = ZkpClient::new(channel);
+struct ZkpClientWrapper {
+    client: ZkpClient<Channel>,
+    username: String,
+    x: BigInt, // The secret
+    p: BigInt,
+    q: BigInt,
+    g: BigInt,
+    h: BigInt,
+    y1: BigInt,
+    y2: BigInt,
+}
 
-    // creating a new Request
-    // let request = tonic::Request::new(
-    //     ParamsRequest {},
-    // );
-    
-    // sending request and waiting for response
-    // let response = client.send(request).await?.into_inner();
-    // println!("RESPONSE={:?}", response);
+impl ZkpClientWrapper {
+    async fn new(addr: &str, username: String, secret: BigInt) -> Result<Self, Box<dyn Error>> {
+        let channel = Channel::from_shared(addr.to_string())?
+            .connect()
+            .await?;
+        let client = ZkpClient::new(channel);
+
+        let mut wrapper = Self {
+            client,
+            username,
+            x: secret,
+            p: BigInt::from(0),
+            q: BigInt::from(0),
+            g: BigInt::from(0),
+            h: BigInt::from(0),
+            y1: BigInt::from(0),
+            y2: BigInt::from(0),
+        };
+
+        wrapper.init_communication().await?;
+        wrapper.register().await?;
+
+        Ok(wrapper)
+    }
+
+    async fn init_communication(&mut self) -> Result<(), Box<dyn Error>> {
+        println!("[INFO] Requesting parameters from server...");
+        let request = tonic::Request::new(ParamsRequest {});
+        let response = self.client.init_communication(request).await?.into_inner();
+        println!("[DEBUG] Received parameters: {:?}", response);
+
+        self.p = BigInt::from_bytes_be(Sign::Plus, &response.p);
+        self.q = BigInt::from_bytes_be(Sign::Plus, &response.q);
+        self.g = BigInt::from_bytes_be(Sign::Plus, &response.g);
+        self.h = BigInt::from_bytes_be(Sign::Plus, &response.h);
+        
+        println!("[DEBUG] - p: {}", self.p.clone());
+        println!("[DEBUG] - q: {}", self.q.clone());
+        println!("[DEBUG] - g: {}", self.g.clone());
+        println!("[DEBUG] - h: {}", self.h.clone());
+        Ok(())
+    }
+
+    async fn register(&mut self) -> Result<(), Box<dyn Error>> {
+        println!("[INFO] Registering user...");
+        self.y1 = self.g.modpow(&self.x, &self.p);
+        self.y2 = self.h.modpow(&self.x, &self.p);
+
+        println!("[DEBUG] Computed y1: {}, y2: {}", self.y1, self.y2);
+        let request = tonic::Request::new(RegisterRequest {
+            username: self.username.clone(),
+            y1: self.y1.to_bytes_be().1,
+            y2: self.y2.to_bytes_be().1,
+        });
+
+        self.client.register(request).await?;
+        println!("[INFO] User registered successfully");
+        Ok(())
+    }
+
+    async fn generate_prove(&mut self) -> Result<String, Box<dyn Error>> {
+        println!("[INFO] Generating proof...");
+        let k = thread_rng().gen_bigint_range(&BigInt::from(0), &self.q);
+        println!("[DEBUG] Generated random number k: {}", k);
+
+        // Compute r1 and r2
+        let r1 = self.g.modpow(&k, &self.p);
+        let r2 = self.h.modpow(&k, &self.p);
+        println!("[DEBUG] Computed r1: {}, r2: {}", r1, r2);
+
+        // Send challenge request
+        let challenge_request = tonic::Request::new(ChallengeRequest {
+            username: self.username.clone(),
+            r1: r1.to_bytes_be().1,
+            r2: r2.to_bytes_be().1,
+        });
+        println!("[DEBUG] Sending challenge request...");
+
+        let challenge_response = self.client.challenge(challenge_request).await?.into_inner();
+        println!("[DEBUG] Received challenge response: {:?}", challenge_response);
+
+        // Compute the response
+        let c = BigInt::from_bytes_be(Sign::Plus, &challenge_response.c);
+        let s = (k - c.clone() * self.x.clone()) % &self.q;
+        println!(
+            "[DEBUG] Computed response c: {}, s: {}",
+            c, s
+        );
+
+        println!("[INFO] verifying proof...");
+        // Send verification request
+        let verify_request = tonic::Request::new(SecretRequest {
+            username: self.username.clone(),
+            s: s.to_bytes_be().1,
+        });
+
+        let verify_response = self.client.verify(verify_request).await?.into_inner();
+        println!("[DEBUG] Received verification response: {:?}", verify_response);
+
+        Ok(verify_response.session)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("[MAIN] Starting client...");
+    let addr = "http://[::1]:50051";
+    let username = "testuser".to_string();
+    let secret = BigInt::from(1234); // This should be a securely generated secret in practice
+    println!("[MAIN] [DEBUG] Initialized parameters username: {} - secret: {}", username, secret);
+
+    let mut client = ZkpClientWrapper::new(addr, username, secret).await?;
+    println!("[MAIN] [DEBUG] Initialized client");
+
+    match client.generate_prove().await {
+        Ok(session) => {
+            if !session.is_empty() {
+                println!("Authentication successful! Session: {}", session);
+            } else {
+                println!("Authentication failed!");
+            }
+        }
+        Err(e) => println!("Error during proof: {}", e),
+    }
+
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::str::FromStr;
 use std::sync::Arc;
 
 use tokio::sync::Mutex;
@@ -8,12 +7,14 @@ use tonic::{transport::Server, Request, Response, Status};
 use zkp_auth::zkp_server::{Zkp, ZkpServer};
 use zkp_auth::{ParamsResponse, ParamsRequest, RegisterRequest, RegisterResponse, ChallengeRequest, ChallengeResponse, SecretRequest, SecretResponse};
 mod zkp_auth;
-use num_traits::{ToBytes};
 
-use rand::{thread_rng, Rng};
+use rand::thread_rng;
 
-use num_bigint::{BigInt, RandBigInt, Sign};
-use is_prime::*;
+use num_bigint::{BigInt, BigUint, RandBigInt, Sign};
+use num_primes::Generator;
+
+use is_prime::is_prime;
+// use num_prime::nt_funcs::is_prime;
 
 
 #[derive(Clone)]
@@ -48,14 +49,8 @@ pub struct ZkpServerI {
 }
 
 fn generate_safe_prime(bit_length: usize) -> BigInt {
-    let mut rng = thread_rng();
-    loop { // Iterates until a safe prime is found
-        let q = rng.gen_biguint((bit_length - 1).try_into().unwrap());
-        let p = &q * 2u32 + 1u32;
-        if is_prime(&q.to_string()) && is_prime(&p.to_string()) {
-            return p.into();
-        }
-    }
+    let p = BigInt::from_biguint(Sign::Plus, BigUint::from_bytes_be(&Generator::safe_prime(bit_length.into()).to_bytes_be()));
+    return p;
 }
 
 fn find_generator(p: &BigInt, q: &BigInt) -> BigInt {
@@ -75,15 +70,21 @@ fn find_generator(p: &BigInt, q: &BigInt) -> BigInt {
 #[tonic::async_trait]
 impl Zkp for ZkpServerI {
     async fn init_communication(&self,request:Request<ParamsRequest>)->Result<Response<ParamsResponse>,Status>{
+        println!("[INFO] init_communication request...");
         // We want to generate P which is a safe prime and q is a subgroup of p. 
         // Safe means that p = 2q + 1, where p and q are both prime numbers
-        let p = generate_safe_prime(2048); // 2048 bits = 256 bytes - standard sha256 size 
+        let p = generate_safe_prime(256); // 256 bits - if bigger it takes more time 
         let q = (&p - 1) / 2; // Sophie Germain prime
+
+        println!("[DEBUG] Generated safe prime p: {}", p);
+        println!("[DEBUG] Generated safe prime q: {}", q);
 
         // Generators should be between 2 and p, and the generator g = 2^((p-1)/q) mod p should be different from 1
         // generators are able to produce any other number in the group p and sub group q
         let g = find_generator(&p, &q);
         let h = find_generator(&p, &q);
+        println!("[DEBUG] Generated safe prime g: {}", g);
+        println!("[DEBUG] Generated safe prime h: {}", h);
 
         let parameters = Parameters { p: p.clone(), q: q.clone(), g: g.clone(), h: h.clone() };
         *self.parameters.lock().await = Some(parameters); // Save the parameters
@@ -95,19 +96,20 @@ impl Zkp for ZkpServerI {
             g: g.to_bytes_be().1,
             h: h.to_bytes_be().1,
         };
+        println!("response: {:?}", response);
 
-        println!("response={:?}", response);
         Ok(Response::new(response))
     }
 
     async fn register(&self, request:Request<RegisterRequest>) -> Result<Response<RegisterResponse>,Status>{
-        println!("request={:?}",request);
+        println!("[INFO] register={:?}",request);
 
         let data = request.get_ref();
         let username = data.username.clone();
         // We could say that y1 and y2 are the public keys of the user
         let y1 = BigInt::from_bytes_be(Sign::Plus, &data.y1);
         let y2 = BigInt::from_bytes_be(Sign::Plus, &data.y2);
+        println!("[DEBUG] Registering user: {}, y1: {}, y2: {}", username, y1, y2);
 
         let user = User {
             id: username.clone(),
@@ -122,18 +124,18 @@ impl Zkp for ZkpServerI {
     }
 
     async fn challenge(&self, request:Request<ChallengeRequest>) -> Result<Response<ChallengeResponse>,Status>{
-        println!("Received challenge request: {:?}", request);
-
         let data = request.get_ref();
         let username = data.username.clone();
         let r1 = BigInt::from_bytes_be(Sign::Plus, &data.r1);
         let r2 = BigInt::from_bytes_be(Sign::Plus, &data.r2);
-    
+        println!("[INFO] Received challenge request, username: {}, r1: {}, r2: {}", username, r1, r2);
+
         let params = self.parameters.lock().await;
     
         // random challenge, generate big int in range of q 
         let c: BigInt = thread_rng().gen_bigint_range(&BigInt::from(0), &params.as_ref().unwrap().q);
-    
+        println!("[DEBUG] Generated random challenge c: {}", c);
+
         let challenge = Challenge {
             id: username.clone(),
             r1: r1.clone(),
@@ -151,38 +153,58 @@ impl Zkp for ZkpServerI {
             c: c.to_bytes_be().1,
         };
     
-        println!("Sending challenge response: {:?}", response);
+        println!("[DEBUG] Sending challenge response: {:?}", response);
         Ok(Response::new(response))    
     }
 
     async fn verify(&self, request:Request<SecretRequest>) -> Result<Response<SecretResponse>,Status>{
-        println!("request={:?}",request);
 
-        // let data = request.get_ref();
-        // let username = data.username.clone();
-        // let s: BigInt = BigInt::from_bytes_be(Sign::Plus, &data.s);
+        let data = request.get_ref();
+        let username = data.username.clone();
+        let s = BigInt::from_bytes_be(Sign::Plus, &data.s);
+        println!("[INFOP] Received verification request, username: {}, s: {}", username, s);
 
-        // let c = &self.challenges.lock().await.get(&username).unwrap();
+        let challenges = self.challenges.lock().await;
+        let challenge = challenges.get(&username).ok_or(Status::not_found("Challenge not found"))?;
+    
+        // Retrieve the user's public keys
+        let users = self.users.lock().await;
+        let user = users.get(&username).ok_or(Status::not_found("User not found"))?;
 
-        // let v1 = (self.parameters.g.modpow(s, &self.parameters.p) *  y1.modpow(c, &self.parameters.p)) % &self.parameters.p;
-        // let v2 = (self.parameters.h.modpow(s, &self.parameters.p) * 
-        //     y2.modpow(c, &self.parameters.p)) % &self.parameters.p;
-        //&v1 == r1 && &v2 == r2;
+        // Retrieve the parameters
+        let parameters = &self.parameters.lock().await;
 
-        let response = SecretResponse{ session: "a".to_string() };
+        let p = &parameters.as_ref().unwrap().p;
+        let g = &parameters.as_ref().unwrap().g;
+        let h = &parameters.as_ref().unwrap().h;
+
+        // verifications for g annd h generators
+        let v1 = (g.modpow(&s, p) * user.y1.modpow(&challenge.c, p)) % p;
+        let v2 = (h.modpow(&s, p) * user.y2.modpow(&challenge.c, p)) % p;
+        println!("[DEBUG] Computed v1: {}, v2: {}", v1, v2);
+
+        let session = if v1 == challenge.r1 && v2 == challenge.r2 {
+            "valid_session_token".to_string() // This should be a random token or something
+        } else {
+            "".to_string()
+        };
+    
+        let response = SecretResponse { session };
+    
+        println!("[DEBUG] Sending verification response: {:?}", response);
         Ok(Response::new(response))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("[INFO] Startint server...");
+    println!("[INFO] Starting server...");
 
     let addr = "[::1]:50051".parse().unwrap();
     let zkp = ZkpServerI::default();
 
-    let request = ParamsRequest{};
-    zkp.init_communication(Request::new(request)).await?;
+    // let request = ParamsRequest{};
+    // zkp.init_communication(Request::new(request)).await?;
     println!("Server listening on {}", addr);
     Server::builder()
         .add_service(ZkpServer::new(zkp))
@@ -190,6 +212,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     Ok(())
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -202,11 +225,18 @@ mod tests {
         let request = Request::new(ParamsRequest {});
         let response = server.init_communication(request).await.unwrap();
         let params = response.get_ref();
-        
-        assert_eq!(BigInt::from_bytes_be(Sign::Plus, &params.p), BigInt::from(0));
-        assert_eq!(BigInt::from_bytes_be(Sign::Plus, &params.q), BigInt::from(0));
-        assert_eq!(BigInt::from_bytes_be(Sign::Plus, &params.g), BigInt::from(0));
-        assert_eq!(BigInt::from_bytes_be(Sign::Plus, &params.h), BigInt::from(0));
+
+        assert!(params.p.len() > 0);
+        assert!(params.q.len() > 0);
+        assert!(params.g.len() > 0);
+        assert!(params.h.len() > 0);
+
+        // Check if p is indeed a safe prime
+        let p = BigInt::from_bytes_be(Sign::Plus, &params.p);
+        let q = BigInt::from_bytes_be(Sign::Plus, &params.q);
+        assert_eq!(&p - 1, &q * 2);
+        assert!(is_prime(&p.to_string()));
+        assert!(is_prime(&q.to_string()));
     }
 
     #[tokio::test]
@@ -214,9 +244,8 @@ mod tests {
         let server = ZkpServerI::default();
         let y1 = BigInt::from(123).to_bytes_be().1;
         let y2 = BigInt::from(456).to_bytes_be().1;
-
         let request = Request::new(RegisterRequest {
-            username: "test".to_string(),
+            username: "testuser".to_string(),
             y1,
             y2,
         });
@@ -225,9 +254,9 @@ mod tests {
         assert_eq!(response.get_ref(), &RegisterResponse::default());
 
         let users = server.users.lock().await;
-        assert!(users.contains_key("test"));
-        let user = users.get("test").unwrap();
-        assert_eq!(user.id, "test");
+        assert!(users.contains_key("testuser"));
+        let user = users.get("testuser").unwrap();
+        assert_eq!(user.id, "testuser");
         assert_eq!(user.y1, BigInt::from(123));
         assert_eq!(user.y2, BigInt::from(456));
     }
@@ -235,10 +264,13 @@ mod tests {
     #[tokio::test]
     async fn test_challenge() {
         let server = ZkpServerI::default();
+        
+        server.init_communication(Request::new(ParamsRequest {})).await.unwrap();
+
         let r1 = BigInt::from(789).to_bytes_be().1;
         let r2 = BigInt::from(101112).to_bytes_be().1;
         let request = Request::new(ChallengeRequest {
-            username: "test".to_string(),
+            username: "testuser".to_string(),
             r1,
             r2,
         });
@@ -249,9 +281,9 @@ mod tests {
         assert!(c > BigInt::from(0));
 
         let challenges = server.challenges.lock().await;
-        assert!(challenges.contains_key("test"));
-        let challenge = challenges.get("test").unwrap();
-        assert_eq!(challenge.id, "test");
+        assert!(challenges.contains_key("testuser"));
+        let challenge = challenges.get("testuser").unwrap();
+        assert_eq!(challenge.id, "testuser");
         assert_eq!(challenge.r1, BigInt::from(789));
         assert_eq!(challenge.r2, BigInt::from(101112));
         assert_eq!(challenge.c, c);
@@ -260,12 +292,33 @@ mod tests {
     #[tokio::test]
     async fn test_verify() {
         let server = ZkpServerI::default();
+        
+        server.init_communication(Request::new(ParamsRequest {})).await.unwrap();
+
+        let y1 = BigInt::from(123).to_bytes_be().1;
+        let y2 = BigInt::from(456).to_bytes_be().1;
+        server.register(Request::new(RegisterRequest {
+            username: "testuser".to_string(),
+            y1,
+            y2,
+        })).await.unwrap();
+
+        let r1 = BigInt::from(789).to_bytes_be().1;
+        let r2 = BigInt::from(101112).to_bytes_be().1;
+        server.challenge(Request::new(ChallengeRequest {
+            username: "testuser".to_string(),
+            r1,
+            r2,
+        })).await.unwrap();
+
+        let s = BigInt::from(42).to_bytes_be().1; 
         let request = Request::new(SecretRequest {
-            username: "test".to_string(),
-            s: vec![1, 2, 3], // Example s value
+            username: "testuser".to_string(),
+            s,
         });
         
         let response = server.verify(request).await.unwrap();
-        assert_eq!(response.get_ref().session, "a");
+        
+        assert!(response.get_ref().session.is_empty() || response.get_ref().session == "valid_session_token");
     }
 }
